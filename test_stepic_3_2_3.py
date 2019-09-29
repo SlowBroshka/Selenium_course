2dfe749ddb1a4f80033d149861206c2633c39626
SlowBroshka <vlad.kuz12@gmail.com>
2019-09-29 16:08:15 +0300

    add @pytest.fixture func

--- test_stepic_3_2_3.py
+++ test_stepic_3_2_3.py
@@ -23,30 +23,36 @@ def click_register(page):
     button = page.find_element_by_css_selector("button.btn")
     button.click()
 
+@pytest.fixture
+def browser():
+    print('\nStart browser')
+    browser = webdriver.Chrome(DRIVER)
+    return browser
+
 
 class TestMainClass:
 
-    def setup_method(self):
-        print('\nstart browser')
-        self.browser = webdriver.Chrome(DRIVER)
+    # def setup_method(self):
+    #     print('\nstart browser')
+    #     self.browser = webdriver.Chrome(DRIVER)
+    #
+    # def teardown_method(self):
+    #     print('\nquit browser')
+    #     self.browser.quit()
 
-    def teardown_method(self):
-        print('\nquit browser')
-        self.browser.quit()
-
-    def test_reg_form1(self):
+    def test_reg_form1(self, browser):
         LINK = 'http://suninjuly.github.io/registration1.html'
-        self.browser.get(LINK)
-        fill_required_fields(self.browser)
-        click_register(self.browser)
+        browser.get(LINK)
+        fill_required_fields(browser)
+        click_register(browser)
         time.sleep(2)
-        welcome_text_elt = self.browser.find_element_by_tag_name("h1")
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
         welcome_text = welcome_text_elt.text
         assert welcome_text == test_expected_text, "Reg test 1 fail actual: {} expected: {}".format(welcome_text, test_expected_text)
 
-    def test_reg_form2(self):
+    def test_reg_form2(self, browser):
         LINK = 'http://suninjuly.github.io/registration2.html'
-        self.browser.get(LINK)
+        browser.get(LINK)
         with pytest.raises(NoSuchElementException):
-            fill_required_fields(self.browser)
+            fill_required_fields(browser)
             pytest.fail("Не должно быть кнопки Отправить")
